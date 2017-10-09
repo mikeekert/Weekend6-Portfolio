df1f7b25073caa0727927a5e8e9aecd2b767b7a0
Mike Ekert <mikeekert@gmail.com>
2017-10-09 09:45:05 -0500

    heroku

--- server/public/scripts/index.controller.js
+++ server/public/scripts/index.controller.js
@@ -1,13 +1,45 @@
-myApp.controller('IndexController', function (GithubService) {
-    
+myApp.controller('IndexController', function (GithubService, $location) {
+
     console.log('IndexController loaded.');
     var vm = this;
     vm.visible = false;
     vm.user = GithubService.user;
     vm.repos = GithubService.repos;
-  
+
     // get profile and repo info on load
     GithubService.githubProfile();
     GithubService.githubRepos();
-  });
-  
+
+    var repoToggle = true;
+    var bioToggle = true;
+
+    vm.toggleRepo = function () {
+        if ($location.path() == '/repo') {
+            repoToggle = false;
+        }
+        console.log('repo false/true:', repoToggle);
+        if (repoToggle) {
+            console.log('repo is true- moving to /repo');
+            $location.url('/repo');
+            repoToggle = !repoToggle;
+        } else {
+            $location.url('/');
+            repoToggle = !repoToggle;
+        }
+    };
+
+    vm.toggleBio = function () {
+        if ($location.path() == '/about') {
+            bioToggle = false;
+            console.log('bio now set to false');
+        }
+        console.log('toggle bio');
+        if (bioToggle) {
+            $location.url('/about');
+            bioToggle = !bioToggle;
+        } else {
+            $location.url('/');
+            bioToggle = !bioToggle;
+        }
+    };
+});
--- server/public/scripts/repo.controller.js
+++ server/public/scripts/repo.controller.js
@@ -1,17 +1,16 @@
 myApp.controller('RepoController', function (GithubService) {
-    console.log('repoController loaded.');
-    var vm = this;
-  
-    vm.user = GithubService.user;
-    vm.repos = GithubService.repos;
-  
-    // get profile and repo info on load
-    GithubService.githubProfile();
-    GithubService.githubRepos();
-  
-    
-    this.date = new Date();
-    
-  
-  });
-  
+  console.log('repoController loaded.');
+  var vm = this;
+
+  vm.user = GithubService.user;
+  vm.repos = GithubService.repos;
+
+  // get profile and repo info on load
+  GithubService.githubProfile();
+  GithubService.githubRepos();
+
+
+  this.date = new Date();
+
+
+});
