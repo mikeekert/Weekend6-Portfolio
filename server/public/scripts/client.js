3e86e8c05abb813d1b7a5a03ed1a8c73ee501613
Mike Ekert <mikeekert@gmail.com>
2017-10-08 20:00:23 -0500

    initial

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,5 @@
+
+
 var myApp = angular.module('myApp', ['ngRoute']);
 
 /// Routes ///
@@ -9,4 +11,6 @@ myApp.config(function($routeProvider, $locationProvider) {
       templateUrl: '/views/user.html',
       controller: 'UserController as uc',
     });
+
+
 });
